74ad898045d4656626984d5fd354918d37f7eb4e
blackie6091 <blackie6091@gmail.com>
2024-01-20 17:08:27 -0500

    Initial commit

---
+++ src/utilities/generalMap.ts
@@ -0,0 +1,4 @@
+export const getGeneralImage = (index: number) => {
+    const generalPaths = ['Alesha.jpeg', 'Aragorn.jpeg', 'Arcades.jpeg', 'Atla.jpeg', 'Avacyn.jpeg', 'Chainer.jpeg', 'Dragon.jpeg', 'Eriette.jpeg', 'Food.jpeg', 'Goreclaw.jpeg', 'Hylda.jpeg', 'Kataki.jpeg', 'Maralen.jpeg', 'Mirko.jpeg', 'Nihiloor.jpeg', 'Ruxa.jpeg', 'Sarulf.jpeg', 'Saskia.jpeg', 'Shelob.jpeg', 'Strefan.jpeg', 'Tazri.jpeg', 'Tinybones.jpeg', 'Torbran.jpeg', 'Vorel.jpeg', 'Wulfgar.jpeg', 'Zur.jpeg'];
+    return generalPaths[index];
+}
