c370e56167032e4ec6a869acb56c8e816b2b1920
blackie6091 <blackie6091@gmail.com>
2024-02-16 11:22:57 -0500

    feat: Add Hallar and adjust image size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,22 +8,22 @@ export default function Home() {
   const [general, setGeneral] = useState(0);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="z-10 max-w-5xl w-full items-center font-mono text-sm flex justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 md:p-6">
+      <div className="items-center justify-center h-auto w-full md:max-w-lg">
             <Image
               src={`/${getGeneralImage(general)}`}
               alt="General Image"
-              className="rounded-[20px] max-h-[600px] max-w-[450px] min-h-[600px] min-w-[450px]"
-              width={450}
-              height={600}
+              className="rounded-[15px] h-auto w-full"
+              width={500}
+              height={500}
               priority
             />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0">
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-auto lg:mb-0">
       <button
-          onClick={() => setGeneral(getRandomInteger(0, 26))}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          onClick={() => setGeneral(getRandomInteger(0, 27))}
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors mt-4 border-gray-600 hover:border-gray-300 hover:bg-gray-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
